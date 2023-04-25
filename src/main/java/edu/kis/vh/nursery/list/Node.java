337c67241102a2f89f15371c2d2d5ebd9676adc9
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-04-25 17:51:57 +0200

    12. Usuwanie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,16 +2,12 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    private int value;
+    private final int value;
     private Node prev;
     private Node next;
 
     protected Node(int i) {
         value = i;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
     }
 
     public int getValue() {
